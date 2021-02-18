20481347c94fbdefa153c999b36f650eef74c766
Darl <dashaapril93@gmail.com>
2021-02-18 23:25:11 +0300

    sections model

---
+++ index.js
@@ -0,0 +1,6 @@
+const loremText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto inventore fugiat cumque totam quaerat impedit deleniti excepturi odit veritatis ad? Tempora placeat cupiditate quas ipsum possimus rem illo facere est.';
+const sections = [
+    {type: 'title', value: 'Hello World!'},
+    {type: 'text', value: loremText},
+    {type: 'columns', value: [loremText, loremText, loremText]}
+]
