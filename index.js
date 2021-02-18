e92d435dcc2edabfe2324440b2ac6f831197c6a2
Darl <dashaapril93@gmail.com>
2021-02-19 00:18:20 +0300

    2 sections added

--- index.js
+++ index.js
@@ -3,4 +3,41 @@ const sections = [
     {type: 'title', value: 'Hello World!'},
     {type: 'text', value: loremText},
     {type: 'columns', value: [loremText, loremText, loremText]}
-]
+];
+
+const $content = document.querySelector('#cv');
+
+//ПРИМЕР ТОГО, КАК ДОБАВИТЬ ТЕКСТ В КОНЕЦ ЭЛЕМЕНТА main
+// sections.forEach(section => {
+//     let html = '<h2>Hello from JS</h2>';
+//     $content.insertAdjacentHTML('beforeend', html)
+// })
+
+sections.forEach(section => {
+    let html = '';
+
+    if (section.type === 'title') {
+        html = `
+        <section class="row">
+        <div class="col-sm">
+          <h2>${section.value}</h2>
+        </div>
+        </section>`;
+
+        
+    } else if (section.type === 'text') {
+        html = `
+        <section class="row">
+          <div class="col-sm">
+            <p>${section.value}</p>
+          </div>
+        </section>`;
+        
+
+    } else if (section.type === 'columns') {
+        html = `
+        `
+
+    }
+    $content.insertAdjacentHTML('beforeend', html)
+});
