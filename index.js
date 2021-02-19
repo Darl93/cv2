da72d6332bf7fd8c79b435fd367e32327c61242c
Darl <dashaapril93@gmail.com>
2021-02-19 22:31:17 +0300

    final_homework

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-const links = [
-    { href: '#hero', icon: 'bi-house-door', value: 'Home' },
-    { href: '#about', icon: 'bi-person', value: 'About' }, 
-    { href: '#resume', icon: 'bi-book', value: 'Resume' }, 
-    { href: '#contacts', icon: 'bi-envelope-open', value: 'Contacts' }, 
-  ];
- 
-  const $navbar = document.querySelector('#navbar');
-  const $navbarUl = document.createElement('ul');
-  $navbar.append($navbarUl);
- 
-  links.forEach(link => {
-      let html = navigation(link);
-      $navbarUl.insertAdjacentHTML('beforeend', html)
-  });
- 
-  function navigation(link) {
-      return `<li><a href="${link.href}" class="nav-link scrollto"><i class="bi ${link.icon}"></i> <span>${link.value}</span></a></li>`;
-  }  
+//ПРИМЕР ТОГО, КАК ДОБАВИТЬ ТЕКСТ В КОНЕЦ ЭЛЕМЕНТА main
+// sections.forEach(section => {
+//     let html = '<h2>Hello from JS</h2>';
+//     $content.insertAdjacentHTML('beforeend', html)
+// })
+
+const loremText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia excepturi commodi impedit, possimus nulla vitae et ex. Quam nisi minima cum natus rem esse voluptatum laborum eveniet sequi. Quo, quibusdam.';
+const sections = [
+    { type: 'title', value: 'Hello world!' },
+   { type: 'text', value: loremText },
+   { type: 'columns', value: [ loremText, loremText, loremText ] }
+];
+
+const $content = document.querySelector('#cv');
+
+sections.forEach(section => {
+    let html = '';
+
+    if (section.type === 'title') {
+        html = title(section);
+    } else if (section.type === 'text') {
+        html = text(section);
+    } else if (section.type === 'columns') {
+        html = columns(section);
+    }
+
+    $content.insertAdjacentHTML('beforeend', html)
+})
+
+function title(section) {
+    return  `
+    <section class="row">
+        <div class="col-sm">
+            <h2>${section.value}</h2>
+        </div>
+    </section>
+`;
+}
+
+function text(section) {
+    return `
+    <section class="row">
+        <div class="col-sm">
+            <p>${section.value}</p>
+        </div>
+    </section>
+    `;
+}
+
+function columns(section) {
+    const html = section.value.map(
+        item => {
+            return `<div class="col-sm"><p>${item}</p></div>`
+        }
+    );
+    console.log(html)
+    return `
+    <section class="row">
+        ${html.join('')}
+    </section>
+    `;
+}
--- navbar.js
+++ navbar.js
@@ -1,75 +1,74 @@
-
+//FIRST SOLUTION
 // let $navbarListUl = document.createElement('ul');
-// let navbarNav = document.querySelector('#navbar');
-// navbarNav.append($navbarListUl);
+//  let navbarNav = document.querySelector('#navbar');
+//  navbarNav.append($navbarListUl);
 
 
-// const navbarItems = [
-//     {type: 'home', value: 'Home'},
-//     {type: 'about', value: 'About'},
-//     {type: 'resume', value: 'Resume'},
-//     {type: 'contacts', value: 'Contacts'},
-// ];
-
-// navbarItems.forEach(item => {
-//     let html = '';
-//     if (item.type === 'home') {
-//       html = home (item);
-//     } else if (item.type === 'about') {
-//       html = about (item);
-//     } else if (item.type === 'resume') {
-//       html = resume (item);
-//     } else if (item.type === 'contacts') {
-//       html = contacts (item);
-//     }
-
-//     $navbarListUl.insertAdjacentHTML('beforeend', html);
-// });
-
-// function home(item) {
-//     return `
-//     <li><a href="#hero" class="nav-link scrollto"><i class="bi bi-house-door"></i> <span>${item.value}</span></a></li>`
-
-// };
-
-// function about(item) {
-//     return `
-//     <li><a href="#about" class="nav-link scrollto"><i class="bi bi-person"></i> <span>${item.value}</span></a></li>`
-// };
-
-// function resume(item) {
-//     return `
-//     <li><a href="#about" class="nav-link scrollto"><i class="bi bi-book"></i><span>${item.value}</span></a></li>`
-// };
-
-// function contacts(item) {
-//     return`
-//     <li><a href="#about" class="nav-link scrollto"><i class="bi bi-envelope-open"></i></i><span>${item.value}</span></a></li>`
-// };
-
-
-
-// //второе решение
-//  const links = [
-//    {href: '#hero', icon: 'bi-house-door', value: 'Home'},
-//    {href: '#about', icon: 'bi-person', value: 'About'},
-//    {href: '#resume', icon: 'bi-book', value: 'Resume'},
-//    {href: '#contacts', icon: 'bi-envelope-open', value: 'Contacts'},
+//  const navbarItems = [
+//      {type: 'home', value: 'Home'},
+//      {type: 'about', value: 'About'},
+//      {type: 'resume', value: 'Resume'},
+//      {type: 'contacts', value: 'Contacts'},
 //  ];
 
-//  const $navbar = document.querySelector('#navbar');
-//  const $navbarUl = document.createElement('ul');
-//  $navbar.append($navbarUl);
+//  navbarItems.forEach(item => {
+//      let html = '';
+//      if (item.type === 'home') {
+//        html = home (item);
+//      } else if (item.type === 'about') {
+//        html = about (item);
+//      } else if (item.type === 'resume') {
+//        html = resume (item);
+//      } else if (item.type === 'contacts') {
+//        html = contacts (item);
+//      }
 
-//  links.forEach(link => {
-//    let html = navigation(link);
-//    $navbarUl.insertAdjacentHTML('beforeend', html);
+//      $navbarListUl.insertAdjacentHTML('beforeend', html);
 //  });
 
-//  function navigation(link) {
-//    return `<li><a href="${link.href}" class="nav-link scrollto"><i class="bi ${link.icon}">`
+//  function home(item) {
+//      return `
+//      <li><a href="#hero" class="nav-link scrollto"><i class="bi bi-house-door"></i> <span>${item.value}</span></a></li>`
+
 //  };
 
+//  function about(item) {
+//      return `
+//      <li><a href="#about" class="nav-link scrollto"><i class="bi bi-person"></i> <span>${item.value}</span></a></li>`
+//  };
+
+//  function resume(item) {
+//      return `
+//      <li><a href="#about" class="nav-link scrollto"><i class="bi bi-book"></i><span>${item.value}</span></a></li>`
+//  };
+
+//  function contacts(item) {
+//      return`
+//      <li><a href="#about" class="nav-link scrollto"><i class="bi bi-envelope-open"></i></i><span>${item.value}</span></a></li>`
+//  };
+
+
+
+//2nd solution
+const links = [
+    { href: '#hero', icon: 'bi-house-door', value: 'Home' },
+    { href: '#about', icon: 'bi-person', value: 'About' }, 
+    { href: '#resume', icon: 'bi-book', value: 'Resume' }, 
+    { href: '#contacts', icon: 'bi-envelope-open', value: 'Contacts' }, 
+  ];
+ 
+  const $navbar = document.querySelector('#navbar');
+  const $navbarUl = document.createElement('ul');
+  $navbar.append($navbarUl);
+ 
+  links.forEach(link => {
+      let html = navigation(link);
+      $navbarUl.insertAdjacentHTML('beforeend', html)
+  });
+ 
+  function navigation(link) {
+      return `<li><a href="${link.href}" class="nav-link scrollto"><i class="bi ${link.icon}"></i> <span>${link.value}</span></a></li>`;
+  }  
 
 
 
